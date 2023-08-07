aaf23c6603318146aae0a977fe745a11deff6508
husnirobani <husnir2005@gmail.com>
2023-08-07 22:01:22 +0700

    Create ImageService

---
+++ app/Services/ImageService.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Services;
+use App\Contracts\Services\ImageServiceInterface;
+use Illuminate\Support\Facades\Storage;
+
+class ImageService implements ImageServiceInterface {
+
+    private string $path;
+    public string  $diskDirectory;
+
+    public object $object;
+    public function __construct(object $object = null)
+    {
+        $this->object = $object;
+    }
+    public function uploadImage($image, $diskDirectory): static
+    {
+        if ($image !== null){
+            $this->path = $image->store($this->diskDirectory);
+            return $this;
+        }
+
+        return $this;
+    }
+
+    public function storeImageDb($fieldName): object
+    {
+        if ($fieldName && $this->path){
+            $this->object->{$fieldName} = $this->path;
+            $this->object->save();
+            return $this->object;
+        }
+
+        return $this;
+    }
+
+    public function deleteImage($fieldName) : static{
+        Storage::delete($this->object->{$fieldName});
+        $this->object->{$fieldName} = 'https://ui-avatars.com/api/?background=3d3d3d&color=fff&rounded=true';
+        $this->object->save();
+        return $this;
+    }
+
+}
