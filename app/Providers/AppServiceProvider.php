47bd39afa77e8a3141fff0fee681adeae46b89d9
husnirobani <husnir2005@gmail.com>
2023-08-07 22:07:43 +0700

    registering two service and two contract class

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Contracts\Services\ImageServiceInterface;
+use App\Contracts\Services\UserServiceInterface;
+use App\Services\ImageService;
+use App\Services\UserService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->app->bind(ImageServiceInterface::class, ImageService::class);
+        $this->app->bind(UserServiceInterface::class, UserService::class);
     }
 }
