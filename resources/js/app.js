f5e39499d953aa71d06bdf9574df81abe96c2eef
husnirobani <husnir2005@gmail.com>
2023-08-12 09:59:35 +0700

    define notivue and configuring that

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,20 +2,33 @@ import './bootstrap';
 import '../css/app.css';
 import 'flowbite';
 
-import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
-
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+import {createApp, h} from 'vue';
+import {createInertiaApp} from '@inertiajs/vue3';
+import {resolvePageComponent} from 'laravel-vite-plugin/inertia-helpers';
+import {ZiggyVue} from '../../vendor/tightenco/ziggy/dist/vue.m';
+import {notivue} from "notivue";
+import "notivue/notifications.css" // Only needed if using built-in notifications
+import "notivue/animations.css" // Only needed if using built-in animations
+const appName = import.meta.env.VITE_APP_NAME || 'Himatif-Area';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
-    setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
+    setup({el, App, props, plugin}) {
+        return createApp({render: () => h(App, props)})
             .use(plugin)
             .use(ZiggyVue, Ziggy)
+            .use(notivue, {
+                position: "top-right",
+                notifications: {
+                    global: {
+                        duration: 10000
+                    },
+                    success: {
+                        title: 'Success'
+                    },
+                },
+            })
             .mount(el);
     },
     progress: {
