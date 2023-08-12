38166ec39a3d4117141c9acabb835663b7f5842f
husnirobani <husnir2005@gmail.com>
2023-08-12 10:14:43 +0700

    i

--- database/migrations/2014_10_11_094453_create_periods_table.php
+++ database/migrations/2014_10_11_094453_create_periods_table.php
@@ -4,17 +4,16 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('periods', function (Blueprint $table) {
-            $table->uuid('id');
+            $table->uuid('id')->primary();
             $table->string('name')->unique();
-            $table->integer('year',)->unique();
+            $table->year('year',)->unique();
             $table->text('description');
             $table->text('vision');
             $table->text('mission');
--- database/migrations/2023_07_27_110618_create_period_users_table.php
+++ database/migrations/2023_07_27_110618_create_period_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -15,8 +14,8 @@ return new class extends Migration
             $table->id();
             $table->timestamps();
             $table->enum('role', ['Admin', 'Admin Period', 'User']);
-            $table->foreignUuid('period_id')->nullable();
-            $table->foreignUuid('user_id');
+            $table->foreignUuid('period_id')->constrained()->cascadeOnDelete();
+            $table->foreignUuid('user_id')->constrained()->cascadeOnDelete();
         });
     }
 
