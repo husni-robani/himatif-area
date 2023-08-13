09916f1b73ff7f4d02e0e7720826ce27803b2c07
husnirobani <husnir2005@gmail.com>
2023-08-13 18:03:03 +0700

    create aspiration_link migration

---
+++ database/migrations/2023_08_13_065416_create_aspiration_links_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('aspiration_links', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->text('link');
+            $table->string('token');
+            $table->string('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('aspiration_links');
+    }
+};
