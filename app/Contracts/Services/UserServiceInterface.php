72993dbede47f1c91ec25c65d88781d3ab6c2a70
husnirobani <husnir2005@gmail.com>
2023-08-13 22:32:54 +0700

    change parameter for UserService.php

--- app/Contracts/Services/UserServiceInterface.php
+++ app/Contracts/Services/UserServiceInterface.php
@@ -1,7 +1,11 @@
 <?php
+
 namespace App\Contracts\Services;
+
+use App\Enums\UserRoleEnum;
 use App\Models\User;
 
-interface UserServiceInterface {
-    public function createUser(\Illuminate\Http\Request $request) : User;
+interface UserServiceInterface
+{
+    public function createUser(\Illuminate\Http\Request $request, UserRoleEnum $role): User;
 }
