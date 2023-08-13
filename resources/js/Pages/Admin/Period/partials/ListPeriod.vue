f9a0ca09f101c5c9cad875dc07f4a2a7e7021e1f
husnirobani <husnir2005@gmail.com>
2023-08-13 09:07:41 +0700

    change dropdown

--- resources/js/Pages/Admin/Period/partials/ListPeriod.vue
+++ resources/js/Pages/Admin/Period/partials/ListPeriod.vue
@@ -5,9 +5,13 @@
           :key="period.year"
           class="pt-3 pb-0 sm:pt-4">
         <div class="flex items-center space-x-4 bg-slate-50 p-2 rounded-lg">
-          <div class="flex-shrink-0">
-            <img alt="Thomas image" class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-5.jpg">
-          </div>
+          <span v-if="period.active === 1" class="relative flex h-3 w-3">
+            <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
+            <span class="relative inline-flex rounded-full h-3 w-3 bg-green-400"></span>
+          </span>
+          <span v-else class="relative flex h-3 w-3">
+            <span class="relative inline-flex rounded-full h-3 w-3 bg-gray-500"></span>
+          </span>
           <div class="flex-1 min-w-0">
             <p class="text-md font-medium text-gray-900 truncate dark:text-white">
               {{ period.name }}
@@ -20,14 +24,12 @@
           <DropdownsDot>
             <template #menu>
               <MenuItem v-slot="{ active }">
-                <Link
-                    :href="route('admin.periods.destroy', period.year)"
-                    as="link"
-                    class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white text-red-500"
-                    method="delete"
+                <button
+                    class="block px-4 py-2 w-full hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white text-red-500"
+                    @click.prevent="deletePeriod(period.year)"
                 >
                   Delete
-                </Link>
+                </button>
               </MenuItem>
               <MenuItem v-slot="{ active }">
                 <Link :href="route('admin.periods.edit', period.year)"
@@ -42,8 +44,12 @@
                 </Link>
               </MenuItem>
               <MenuItem v-slot="{ active }">
-                <a class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                   href="#">Set As Active</a>
+                <button
+                    v-show="period.active === 0"
+                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                    @click.prevent="changeActiveStatus(period.year)">
+                  Set As Active
+                </button>
               </MenuItem>
             </template>
           </DropdownsDot>
@@ -56,13 +62,40 @@
 </template>
 
 <script setup>
-import {Link} from "@inertiajs/vue3";
+import {Link, useForm} from "@inertiajs/vue3";
 import DropdownsDot from "@/Components/DropdownsDot.vue";
 import {MenuItem} from "@headlessui/vue";
+import {usePush} from "notivue";
 
-document.addEventListener("DOMContentLoaded", function (event) {
-  document.getElementById('defaultModalButton').click();
-});
+const formDelete = useForm({})
+const push = usePush();
+
+function changeActiveStatus(year) {
+  useForm({}).post(route('admin.periods.activeStatus', year), {
+    onSuccess: () => {
+      push.success({
+        message: 'Active period has been changed'
+      })
+    }
+  })
+}
+
+function deletePeriod(year) {
+  formDelete.delete(route('admin.periods.destroy', year), {
+    onSuccess: () => {
+      push.warning({
+        title: 'Delete Success',
+        message: 'period ' + year + ' has been deleted',
+      });
+    },
+    onError: () => {
+      push.error({
+        message: 'period failed to Delete'
+      })
+    }
+  })
+
+}
 
 const props = defineProps({
   periods: {
