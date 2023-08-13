c794793a62869d3e3ad5e9022bc53a3d01f24012
husnirobani <husnir2005@gmail.com>
2023-08-13 09:07:18 +0700

    create method update for receiving update period and method changeActive for change active status period

--- app/Http/Controllers/Admin/PeriodController.php
+++ app/Http/Controllers/Admin/PeriodController.php
@@ -29,7 +29,7 @@ class PeriodController extends Controller
             $period->mission = $request->mission;
             $period->save();
         } catch (\Exception $exception) {
-            return to_route('admin.periods.index')->with('failed', 'failed to create new period');
+            return to_route('admin.periods.index');
         }
 
         return to_route('admin.periods.index');
@@ -40,12 +40,47 @@ class PeriodController extends Controller
         $period = Period::where('year', $year)->first();
         $period->delete();
 
-        return back();
+        return to_route('admin.periods.index')->with('success', 'Deleting period success');
     }
 
     public function edit(string $year)
     {
         $period = Period::where('year', $year)->first();
-        dd($period);
+        return Inertia::render('Admin/Period/Edit', compact('period'));
+    }
+
+    public function update(Request $request, $year)
+    {
+        try {
+            $period = Period::where('year', $year)->first();
+            $period->name = $request->name;
+            $period->year = $request->year;
+            $period->description = $request->description;
+            $period->vision = $request->vision;
+            $period->mission = $request->mission;
+            $period->save();
+        } catch (\Exception $exception) {
+            return $exception;
+        }
+
+        return to_route('admin.periods.index');
+    }
+
+    public function changeActive($year)
+    {
+
+        try {
+            $activePeriod = Period::where('active', 1)->first();
+            $activePeriod->active = 0;
+            $activePeriod->save();
+
+            $period = Period::where('year', $year)->first();
+            $period->active = 1;
+            $period->save();
+        } catch (\Exception $exception) {
+            return $exception;
+        }
+
+        return to_route('admin.periods.index');
     }
 }
